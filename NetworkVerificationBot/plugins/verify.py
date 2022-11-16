423fe1f498c6a03b83d2f6a68c4eab6861e10aa4
NoobStark-11 <115748499+NoobStark-11@users.noreply.github.com>
2022-11-16 09:07:35 +0530

    Update verify.py

--- NetworkVerificationBot/plugins/verify.py
+++ NetworkVerificationBot/plugins/verify.py
@@ -42,7 +42,7 @@ async def nope(_, CallbackQuery):
 @app.on_callback_query(filters.regex("yes_tag"))
 async def yos(_, CallbackQuery):
     query = CallbackQuery.message
-    await query.edit_text("great\nclick /proceed to continue the verification"")
+    await query.edit_text("great\nclick /proceed to continue the verification")
 
 @app.on_callback_query(filters.regex("no_tag"))
 async def nope(_, CallbackQuery):
