d63463c2b3680257051818a6a2d3a80b5c82bd92
NoobStark-11 <115748499+NoobStark-11@users.noreply.github.com>
2022-11-16 07:48:06 +0530

    Update __main__.py

--- NetworkVerificationBot/__main__.py
+++ NetworkVerificationBot/__main__.py
@@ -7,7 +7,7 @@ HEY THIS IS A VERIFICATION FORM
 @app.on_message(filters.command(start))
 async def start(_, msg):
     await msg.reply_photo(
-            photo="NETWORK_IMG",
+            photo=NETWORK_IMG,
             caption=START_MSG
             )
 if __name__ == "__main__" :
