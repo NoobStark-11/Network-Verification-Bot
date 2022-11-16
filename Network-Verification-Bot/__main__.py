2b5226a6ce183578aad0e60a30e334460e6f695e
NoobStark-11 <115748499+NoobStark-11@users.noreply.github.com>
2022-11-16 07:41:40 +0530

    Update __main__.py

--- Network-Verification-Bot/__main__.py
+++ Network-Verification-Bot/__main__.py
@@ -1,1 +1,12 @@
+from Network-Verification-Bot import app, NETWORK_IMG
 
+START_MSG="""
+HEY THIS IS A VERIFICATION FORM
+"""
+
+@app.on_message(filters.command(start))
+async def start(_, msg):
+    await msg.reply_photo(
+            photo="NETWORK_IMG",
+            caption=START_MSG"
+            )
