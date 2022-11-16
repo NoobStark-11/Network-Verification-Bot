5811190e480acde4f2786640edec493ab74c6ce1
NoobStark-11 <115748499+NoobStark-11@users.noreply.github.com>
2022-11-16 12:35:09 +0530

    Update __main__.py

--- NetworkVerificationBot/__main__.py
+++ NetworkVerificationBot/__main__.py
@@ -1,12 +1,14 @@
 from pyrogram import filters
 from NetworkVerificationBot import app, NETWORK_IMG
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+
 START_MSG="""
-ʜᴇʏ ɪ ᴀᴍ {} ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ ʙᴏᴛ,
+ʜᴇʏ {} ɪ ᴀᴍ {} ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ ʙᴏᴛ,
 ᴀᴍ ʜᴇʀᴇ ᴛᴏ ᴠᴇʀɪғʏ ʏᴏᴜ ɪɴ ᴍʏ ɴᴇᴛᴡᴏʀᴋ.
 ɪғ ʏᴏᴜ ᴀʀᴇ ʀᴇᴀᴅʏ ᴛᴏ ɢᴇᴛ ᴠᴇʀɪғɪᴇᴅ ɪɴ ʏᴏᴜʀ ɴᴇᴛᴡᴏʀᴋ 
 ᴋɪɴᴅʟʏ ᴜsᴇ /verify ᴛᴏ ᴠᴇʀɪғʏ ʏᴏᴜʀsᴇʟғ.
 """
+
 buttons = [
      [
        InlineKeyboardButton (text="ʜᴇᴀᴅǫᴜᴀᴛᴇʀs",url="https://t.me/+lUUS9nd1HxozYjBl"),
@@ -22,7 +24,7 @@ buttons = [
 async def start(_, msg):
     await msg.reply_photo(
             photo=NETWORK_IMG,
-            caption=START_MSG.format(msg.from_user.first_name),
+            caption=START_MSG.format(msg.from_user.first_name, NETWORK_NAME),
             reply_markup=InlineKeyboardMarkup (buttons)
             )
 
