2b00ae24ed80f55763ddd44e7fc9f4adee063158
NoobStark-11 <115748499+NoobStark-11@users.noreply.github.com>
2022-11-16 08:08:57 +0530

    Update __main__.py

--- NetworkVerificationBot/__main__.py
+++ NetworkVerificationBot/__main__.py
@@ -3,13 +3,27 @@ from NetworkVerificationBot import app, NETWORK_IMG
 
 START_MSG="""
 HEY THIS IS A VERIFICATION FORM
+click /verify to verify yourself in our network 
 """
+buttons= 
+     [
+     [
+       InlineKeyboardButton (text="headquarters",url="https://t.me/+lUUS9nd1HxozYjBl")
+       InlineKeyboardButton (text="approved forms",url="https://t.me/+lUUS9nd1HxozYjBl")
+     ]
+     [
+       InlineKeyboardButton (text="network",url="https://t.me/+lUUS9nd1HxozYjBl")
+     ]
+     ]
+
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(_, msg):
     await msg.reply_photo(
             photo=NETWORK_IMG,
-            caption=START_MSG
+            caption=START_MSG,
+            reply_markup=InlineKeyboardMarkup (buttons)
             )
+
 if __name__ == "__main__" :
     app.run()
