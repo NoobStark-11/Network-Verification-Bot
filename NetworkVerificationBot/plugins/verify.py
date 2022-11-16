2c03fd7ee6ba074acc971fc2a9c7d5b7d0f0e37c
NoobStark-11 <115748499+NoobStark-11@users.noreply.github.com>
2022-11-16 10:43:43 +0530

    Update verify.py

--- NetworkVerificationBot/plugins/verify.py
+++ NetworkVerificationBot/plugins/verify.py
@@ -35,7 +35,8 @@ async def nope(_, CallbackQuery):
 @app.on_message(filters.command("continue"))
 async def verify(client,msg):
     id=msg.chat.id
-    user = await app.get_users(user)
+    ff = m.from_user
+    user = await app.get_users(ff)
     id1=user.id
     a = await client.ask(id,"Mind putting our tag on your name?")
     b = await client.ask(id,"Write the reason(s) for joining our network.")
