64f266a0b4a5fc44adfdbd418b03a64aaacc2c4d
NoobStark-11 <115748499+NoobStark-11@users.noreply.github.com>
2022-11-16 11:36:49 +0530

    Update verify.py

--- NetworkVerificationBot/plugins/verify.py
+++ NetworkVerificationBot/plugins/verify.py
@@ -14,33 +14,9 @@ VERIFY_BUTTONS= [
           InlineKeyboardButton (text="no,i decline",callback_data="no_verify")        
          ],
       ]
-first_name = msg.from_user.first_name
-username = msg.from_user.username
-user_id = msg.from_user.id
-dc_id = msg.from_user.dc_id
-last_online_date = msg.from_user.last_online_date
 
-VERIFICATION_FORM=f"""
-FIRST NAME: {first_name}
-USERNAME: {username}
-ID: {user_id}
-DC ID : {dc_id}
-LAST ONLINE: {last_online_date}
 
-CAN PUT TAG : {a.text}
-REASON FOR JOINING: {b.text}
-FULL NAME : {c.text}
-AGE : {d.text}
-GENDER : {e.text}
-COUNTRY : {f.text}
-GITHUB LINK : {g.text}
-PROGRAMING LANGUAGES: {h.text}
-CHANNELS: {i.text}
-GROUPS : {j.text}
-BOTS : {k.text}
-SKILLS: {l.text}
-ABOUT THEM : {m.text}
-"""
+
 @app.on_message(filters.command("verify"))
 async def verify(_,msg):
     await msg.reply_text(VERIFY_MSG,
@@ -61,7 +37,12 @@ async def nope(_, CallbackQuery):
 
 @app.on_message(filters.command("continue"))
 async def verify(client,msg):
-    id=msg.chat.id    
+    id=msg.chat.id   
+    first_name = msg.from_user.first_name
+    username = msg.from_user.username
+    user_id = msg.from_user.id
+    dc_id = msg.from_user.dc_id
+    last_online_date = msg.from_user.last_online_date 
     photo = msg.from_user.photo.big_file_id
     media =  await app.download_media(message=photo)
     
@@ -79,7 +60,28 @@ async def verify(client,msg):
     l = await client.ask(id,"Your skills:")
     m = await client.ask(id,"Tell me about yourself in one message:")
     await app.send_message(VERIFICATION_CHANNEL_ID,f"hii")
-    await msg.reply_photo(photo=media, caption=VERIFICATION_FORM,
+    await msg.reply_photo(photo=media, 
+       caption=f"""
+FIRST NAME: {first_name}
+USERNAME: {username}
+ID: {user_id}
+DC ID : {dc_id}
+LAST ONLINE: {last_online_date}
+
+CAN PUT TAG : {a.text}
+REASON FOR JOINING: {b.text}
+FULL NAME : {c.text}
+AGE : {d.text}
+GENDER : {e.text}
+COUNTRY : {f.text}
+GITHUB LINK : {g.text}
+PROGRAMING LANGUAGES: {h.text}
+CHANNELS: {i.text}
+GROUPS : {j.text}
+BOTS : {k.text}
+SKILLS: {l.text}
+ABOUT THEM : {m.text}
+""",
        )
     os.remove(photo)
 
