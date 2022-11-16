486ee1d4fdbe695e060cd8499d03d2f9d89c4898
NoobStark-11 <115748499+NoobStark-11@users.noreply.github.com>
2022-11-16 16:16:02 +0530

    Update info.py

--- NetworkVerificationBot/utils/info.py
+++ NetworkVerificationBot/utils/info.py
@@ -1,8 +1,9 @@
 from NetworkVerificationBot import app
 
-async def info(_,msg):    
-        first_name = msg.from_user.first_name
-        username = msg.from_user.username
-        user_id = msg.from_user.id
-        dc_id = msg.from_user.dc_id
-        return first_name,username,user_id,dc_id
+def info(_,msg):    
+    first_name = msg.from_user.first_name
+    username = msg.from_user.username
+    user_id = msg.from_user.id
+    dc_id = msg.from_user.dc_id
+    return first_name,username,user_id,dc_id
+        
