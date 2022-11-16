2bc102fda356957085e1115aa2c121aace0fd924
NoobStark-11 <115748499+NoobStark-11@users.noreply.github.com>
2022-11-16 15:30:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     API_ID=16191628
     API_HASH="7d5acccaf1df4f5b7a690b203fd1953e"
     TOKEN="5651385902:AAFwigsgZLEETFCXutMjLyfMNgwuaUjMTIU"
-    NETWORK_IMG="https://telegra.ph/file/2c7a09bff109deec3e305.jpg"
+    START_IMG="https://telegra.ph/file/2c7a09bff109deec3e305.jpg"
     TOS_LINK="https://t.me/c/1547036942/1021"
     VERIFICATION_CHANNEL_ID=-1001715350671
     NETWORK_NAME = "ok"
@@ -12,3 +12,4 @@ class Config:
     VERIFICATION_CHANNEL_ID = -1001715350671
     APPROVED_CHANNEL_ID = -1001673903836
     ADMINS=5264285143
+    NETWORK_IMG="https://telegra.ph/file/2c7a09bff109deec3e305.jpg"
